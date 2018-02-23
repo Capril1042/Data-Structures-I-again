d185eb5b870e7348d591a711c78a41670c2061bf
TaiTheThai <tai@taithethai.com>
2018-02-23 09:34:53 -0800

    queue completed

--- src/queue.js
+++ src/queue.js
@@ -1,3 +1,5 @@
+const Stack = require('./Stack');
+
 /*
   1. Add a constructor with a storage structure; there are multiple options you could use for this
   2. Add a size getter that returns the number of items the queue is storing
@@ -5,7 +7,25 @@
   4. Add a `dequeue` method that removes the item in the queue that was added earliest
 */
 class Queue {
-
+  constructor() {
+    this.storageIn = new Stack();
+    this.storageOut = new Stack();
+  }
+  get size() {
+    return this.storageIn.size + this.storageOut.size;
+  }
+  enqueue(value) {
+    this.storageIn.push(value);
+  }
+  dequeue() {
+    if (this.storageOut.size === 0) {
+      while (this.storageIn.size > 0) {
+        this.storageOut.push(this.storageIn.pop());
+      }
+    }
+    // return this.storageOut.pop();
+    return this.size ? this.storageOut.pop() : undefined;
+  }
 }
 
 module.exports = Queue;
